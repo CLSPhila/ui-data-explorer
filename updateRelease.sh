5e58164e1488a52a16a1a30b7ca09d01c34f9998
Nate Vogel <nathan.c.vogel@gmail.com>
2020-07-31 14:33:39 -0400

    finished updateRelease script, I think

--- updateRelease.sh
+++ updateRelease.sh
@@ -4,35 +4,71 @@
 # note - to run using env vars in the running shell,run with `. .updateRelease`
 release_name=uiExplorerData
 asset_name=unemployment_data.parquet
-
+owner=clsphila
+repository=ui-data-explorer
 
 echo "Updating release $release_name"
 
+function upload_asset {
+  release_id=$1
+  echo "Uploading asset to $release_id"
+  uploaded_response=$(curl \
+    -X POST \
+    -u natev:"$GITHUB_TOKEN" \
+    -H "Accept:application/vnd.github.v3+json" \
+    -H "Content-Type:application/octet-stream" \
+    --data-binary @"data/unemployment_data.parquet" \
+    "https://uploads.github.com/repos/$owner/$repository/releases/$release_id/assets?name=$asset_name"
+    )
+  echo "finished uploading asset."
+}
 
 function create_release {
   echo "Release not found. Creating."
   new_release=$(curl \
     -u natev:"$GITHUB_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
-     https://api.github.com/repos/clsphila/ui-data-explorer/releases \
+     "https://api.github.com/repos/$owner/$repository/releases" \
     -d "{\"tag_name\":\"$release_name\", \"name\":\"$release_name\"}"
     )
   echo "new release: $new_release"
-  assets_url=$(jq '.assets_url' <<< $new_release)
+  release_id=$(jq '.id' <<< $new_release)
   echo "Assets url: $assets_url"
-  uploaded_response=$(curl \
-    -X POST \
+  echo "end of assets url"
+  upload_asset $release_id
+}
+
+function clear_assets {
+  $assets=$1
+  for row in $(echo "${assets}" | jq -r '.[] | @base64'); do
+    _jq() {
+     echo ${row} | base64 --decode | jq -r ${1}
+    }
+    asset_id="$(_jq '.id')"
+    echo "Delete Asset: $asset_id"
+    deleted=$(curl \
+    -X DELETE \
     -u natev:"$GITHUB_TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
-    -H "Content-Type: application/octet-stream" \
-    "$assets_url" \
-    --data-binary @"ui-data-explorer/data/unemployment_data.parquet"
+     "https://api.github.com/repos/$owner/$repository/releases/assets/$asset_id"
     )
-  echo "$uploaded_response"
+  done 
 }
 
 function replace_asset {
   echo "Replacing asset."
+  release=$1
+  release_id=$(jq '.id' <<< "$release")
+  assets=$(jq '.assets' <<< "$release")
+  asset_count=$(jq '. | length'  <<< "$assets")  
+  if [ $asset_count != "0" ]; then
+    echo "need to delete assets."
+    clear_assets $assets
+  fi
+
+  assets_url=$(jq '.assets_url' <<< "$release")
+  release_id=$(jq '.id' <<< "$release")
+  upload_asset $release_id
 }
 
 # get the releases
@@ -40,16 +76,13 @@ releases_complete=$(curl \
   -H "Accept: application/vnd.github.v3+json" \
   https://api.github.com/repos/clsphila/ui-data-explorer/releases)
 
-echo "all releases: $releases_complete"
 
 # find the release we care about with its name.
-releases=$(jq --arg release_name "$release_name" '[.[] | select(.name==$release_name) | {id: .id, assets_url: .assets_url, name: .name, assets: .assets}]'  <<< "$releases_complete")
+releases=$(jq --arg release_name "$release_name" '[.[] | select(.name==$release_name) | {id: .id, upload_url: .upload_url, assets_url: .assets_url, name: .name, assets: .assets}]'  <<< "$releases_complete")
 
-echo "releases filtered: $releases"
 
 # if this release doesn't exist, create it and upload the asset.
 length=$(jq '. | length' <<< "$releases")
-echo "$length"
 
 if [ $length == "0" ];
 then
@@ -57,32 +90,14 @@ then
 else
   if [ $length == "1" ];
   then
-    replace_asset 
+    release=$(jq '.[0]' <<< "$releases")
+    replace_asset "$release"
   else
-    echo "More than one release found. Not sure what to do. Exiting." 
+    echo "More than one release named $release_name found. Not sure what to do. Exiting." 
   fi
 fi
 
 
 
-release_id=$(jq '.[] | .id' <<< "$releases")
-assets_url=$(jq '.[] | .assets_url' <<< "$releases")
-
-#echo "$releases"
-echo "$assets_url"
-echo "$release_id"
-
-# find the asset_id for the asset we are replacing.
-
-
-# delete the asset we care about replacing.
-
-
-# upload the replacement asset.
-#uploaded=$(curl \
-#    -u nvogel:"$GITHUB_TOKEN"
-#    -X POST 
-#    -H "Accept: application/vnd.github.v3+json"
-#    -H "Content-Type: application.octet-stream"
-    
+   
 
