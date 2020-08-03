dcb1ef5a690031118dfe82510e9760ffe922bfa1
Nate Vogel <nathan.c.vogel@gmail.com>
2020-08-03 13:39:23 -0400

    fix typo in deployshinyapps.R

--- deployShinyApps.R
+++ deployShinyApps.R
@@ -5,7 +5,7 @@
 
 message("Deploying to shinyapps.")
 rsconnect::setAccountInfo(name=Sys.getenv("SHINYAPPS_ACCOUNT"), token=Sys.getenv("SHINYAPPS_TOKEN"), secret=Sys.getenv("SHINYAPPS_SECRET"))
-rsconnect::deployApp(Sys.getenv("PROJECT_ROOT"),
+rsconnect::deployApp(config::get("PROJECT_ROOT"),
                      appName="ui-data-explorer",
                      appFileManifest=file.path(config::get("PROJECT_ROOT"), 'filemanifest.txt'), 
                      launch.browser = FALSE)
